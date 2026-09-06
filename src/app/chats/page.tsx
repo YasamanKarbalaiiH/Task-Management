bfeb39feb62a788401de6d155dc7a76f6378b6d0
YasamanKarbalaii <yasamankarbalaii@gmail.com>
2026-09-06 17:51:29 +0330

    Create page.tsx

---
+++ src/app/chats/page.tsx
@@ -0,0 +1,17 @@
+import CreateTask from "../components/CreateTask";
+
+async function Page() {
+  return (
+    <main className="min-h-screen bg-background px-4 py-5 md:px-6 lg:px-8">
+      <div className="mx-auto max-w-[1600px]">
+        <div className="mb-6">
+          <CreateTask title="Chats" />
+        </div>
+
+        <section></section>
+      </div>
+    </main>
+  );
+}
+
+export default Page;
