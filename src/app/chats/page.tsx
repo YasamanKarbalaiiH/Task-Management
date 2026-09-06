388ebcd8ecce5e5ce62b35fa0b70e86a2f817f6b
YasamanKarbalaii <yasamankarbalaii@gmail.com>
2026-09-06 19:00:29 +0330

    Update page.tsx

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -1,8 +1,11 @@
-import Chat from "../components/chats/Chat";
 import CreateTask from "../components/CreateTask";
+import Message from "../api/Message";
+import Users from "../api/User";
+import Chat from "../components/chats/Chat";
 
 async function Page() {
-  const message = await 
+  const messages = await Message();
+  const users = await Users();
   return (
     <main className="min-h-screen bg-background px-4 py-5 md:px-6 lg:px-8">
       <div className="mx-auto max-w-[1600px]">
