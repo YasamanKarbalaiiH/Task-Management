29e9f33318cb10c9699577444b1acd7b4529a35f
YasamanKarbalaii <yasamankarbalaii@gmail.com>
2026-09-09 12:53:25 +0330

    Update page.tsx

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import CreateTask from "../components/CreateTask";
 
 function page() {
-  return <div></div>;
+  return (
+    <div>
+      <section>
+        <CreateTask title="Notifications" />
+      </section>
+    </div>
+  );
 }
 
 export default page;
