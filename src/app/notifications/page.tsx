2e0770530e116b2632b386ca1469caa651a2670b
YasamanKarbalaii <yasamankarbalaii@gmail.com>
2026-09-12 11:56:17 +0330

    main

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -1,11 +1,15 @@
 import CreateTask from "../components/CreateTask";
 import Alart from "../api/Alart";
+import Notif from "../components/notif/Notif";
 async function page() {
   const notifs = await Alart();
   return (
     <div>
       <section>
         <CreateTask title="Notifications" />
+      </section>
+      <section>
+        <Notif data={notifs} />
       </section>
     </div>
   );
