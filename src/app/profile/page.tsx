fde564ba19e437da7453ae6df76d9625b49b03fe
YasamanKarbalaii <yasamankarbalaii@gmail.com>
2026-09-12 15:57:42 +0330

    Update page.tsx

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,15 @@
 import CreateTask from "../components/CreateTask";
-
+import Profile from "../components/profile/Profile";
+import Profiledata from "../api/Profile";
 async function page() {
+  const data = await Profiledata();
   return (
     <div className="min-h-screen bg-background px-4 py-5 md:px-6 lg:px-8">
       <section>
         <CreateTask title="Profile" />
+      </section>
+      <section>
+        <Profile data={data} />
       </section>
     </div>
   );
